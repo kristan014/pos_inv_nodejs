53c170806ecd693f701b80c0e03b47512a27ffb1
Bryan Bautista <bautistabryan2k19@gmail.com>
2021-07-24 19:20:44 +0800

    d

--- READmeFIRST.txt
+++
@@ -1,7 +0,0 @@
-1. Create a database with a name of "pos_with_is" in phpmyadmin
-
-2. Clone with HTTPS to get the files into your local
-
-3. npm install
-
-4. npm run dev
--- src/routes/login.route.js
+++
@@ -1,8 +0,0 @@
-var router = require("express").Router();
-
-const loginController = require("../controllers/login.controller");
-
-router.post("/", loginController.login);
-router.post("/send-code", loginController.sendCode);
-
-module.exports = router;
